09b5a9c25a5572c54fb1ec7f3453bf4a8dcb2f71
Shub_S <newc217621@gmail.com>
2026-08-02 16:29:56 +0530

    Day 18: Hashing - Two Sum using HashMap

---
+++ Day18_Hashing_TwoSum/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[]{seen.get(complement), i};
+            }
+            seen.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
---
+++ Day18_Hashing_TwoSum/README.md
@@ -0,0 +1,16 @@
+# Day 18 - HashMap & HashSet Deep Dive: Two Sum Pattern
+
+## Problem
+Find indices of two numbers in an array that add up to a target.
+
+## Concepts Covered
+- HashMap internals (buckets, hashCode, O(1) average operations)
+- HashSet vs HashMap
+- The "Two Sum" hashing pattern (check-before-insert)
+- O(n^2) brute force to O(n) optimization
+
+## Files
+- Main.java
+
+## Complexity
+Time: O(n) | Space: O(n)
